e8131aef30e16adcb3803b5b55b5a5eb5dc62fa7
Mr-Xibon94 <md19jibon94@gmail.com>
2026-04-16 08:38:57 +0600

    completed footersection

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -30,6 +30,16 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
+                <div className='flex justify-between text-gray-400 border-t border-gray-600 mt-8 pt-2.5'>
+                    <div>
+                        <p>&copy; 2026 KeenKeeper. All rights reserved.</p>
+                    </div>
+                    <div className='flex  gap-2.5'>
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Cookies</p>
+                    </div>
+                </div>
                     
                 </div>
             </div>
